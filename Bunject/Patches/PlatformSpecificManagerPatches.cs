0591738bbba08ce185ddf433b5b923b43f811064
Serge Azel <SergeAzel@gmail.com>
2023-09-02 17:45:08 -0500

    Achievement unlocking prevention measures

---
+++ Bunject/Patches/PlatformSpecificManagerPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using PlatformSpecific;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunject.Patches.PlatformSpecificManagerPatches
+{
+  [HarmonyPatch(typeof(PlatformSpecificManager), nameof(PlatformSpecificManager.UnlockAchievement))]
+  internal class UnlockAchievementPatches
+  {
+    static bool Prefix()
+    {
+      // FORCEFULLY prevent any achievement unlocking - mods should not ever unlock platform achievements.
+      return false;
+    }
+  }
+
+  [HarmonyPatch(typeof(PlatformSpecificManager), nameof(PlatformSpecificManager.CheckAchievementsOnStart))]
+  internal class CheckAchievementsOnStartPatches
+  {
+    static bool Prefix()
+    {
+      // FORCEFULLY prevent any achievement unlocking - mods should not ever unlock platform achievements.
+      return false;
+    }
+  }
+}
